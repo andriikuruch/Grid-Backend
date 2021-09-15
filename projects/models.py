360639b43e338c0d4668141e38db783b2e5ad873
andriikuruch <andriikuruch@gamil.com>
2021-09-15 15:57:30 +0300

    remove Style oneToOneField

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,7 @@ from cloudinary.models import CloudinaryField
 class Project(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField()
-    style_id = models.OneToOneField('Style', on_delete=models.CASCADE, related_name='style')
+    # style_id = models.OneToOneField('Style', on_delete=models.CASCADE, related_name='style')
     aria = models.PositiveSmallIntegerField()
     designer = models.CharField(max_length=150)
     address = models.CharField(max_length=250)
